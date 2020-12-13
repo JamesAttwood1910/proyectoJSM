c260b5ff8a718e08759b5d2b923bd3c6e84bf311
JamesAttwood1910 <attwood1910@gmail.com>
2020-12-13 15:44:50 -0300

    Grafico Lolipop

--- 1.GraficosEjemplos/2. Grafico Lollipop/Grafico_lollipop.R
+++ 1.GraficosEjemplos/2. Grafico Lollipop/Grafico_lollipop.R
@@ -5,10 +5,11 @@
 library(readr)
 library(ggplot2)
 library(dplyr)
+library(scales)
 
 # Crear base de datos
 
-a <- data_frame(
+a <- tibble(
   Año = c("2019", "2018", "2017", "2014", "2010", "2005",
           "2002", "1992", "1982",
           "1970", "1940", "1920", "1907","1885"),
@@ -30,16 +31,18 @@ a <- a %>%
 
 ggplot(data = a, aes(x = Año, y = Población)) + 
   geom_segment(aes(x = Año, xend = Año, y = 0, yend = Población), 
-               color = ifelse(a$Año %in% c("2017"), "#1B9E77", "#66A61E"), 
+               color = ifelse(a$Año %in% c("2017"), "#EF3D14", "#054762"), 
                size = ifelse(a$Año %in% c("2017"), 2, 0.7)) + 
-  geom_point(color = ifelse(a$Año %in% c("2017"), "#1B9E77", "#66A61E"),
+  geom_point(color = ifelse(a$Año %in% c("2017"), "#EF3D14", "#054762"),
              size = ifelse(a$Año %in% c("2017"), 3, 1)) +
-  coord_flip() + ylim(0, 2000000) + 
+  coord_flip() +  
+  theme_minimal() + 
   annotate(geom = "label", x = grep("2017", a$Año), y = a$Población[which(a$Año=="2017")]*1.7,
            label = paste("Census 2017 \n Población =", a$Población[which(a$Año=="2017")]),
-           fill = "grey") +
-  ggtitle("Titulo") + 
-  theme_minimal()
+           fill = "#16C3CE") +
+  scale_y_continuous(labels = comma_format(big.mark = ","), limits = c(0,2000000)) +
+  labs(title = "Poplación inmigrante en Chile", caption = "Fuente: XXXX" )
+
 
 # Como agregar mas diseños. 
 
